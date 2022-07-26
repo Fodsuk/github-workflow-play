ca17e016688bebf691f121f8a93bf686b7904796
michael rodda <michael.rodda@schroders.com>
2022-07-26 15:12:47 +0100

    run tests

---
+++ src/outputs.tf
@@ -0,0 +1,7 @@
+output "sql_server_resource_group_name" {
+  value = azurerm_mssql_server.schroders.resource_group_name
+}
+
+output "sql_server_minimum_tls_version" {
+    value = azurerm_mssql_server.schroders.minimum_tls_version
+}
--- src/resource-sql_server.tf
+++ src/resource-sql_server.tf
@@ -1,4 +1,4 @@
-resource "azurerm_mssql_server" "example" {
+resource "azurerm_mssql_server" "schroders" {
   name                         = "schroderssql"
   resource_group_name          = var.resource_group_name
   location                     = var.location
