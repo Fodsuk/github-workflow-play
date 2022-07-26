7ae83ec071c0da4786cc2186529e344819b1b428
michael rodda <michael.rodda@schroders.com>
2022-07-26 15:14:57 +0100

    fmt

--- src/outputs.tf
+++ src/outputs.tf
@@ -3,5 +3,5 @@ output "sql_server_resource_group_name" {
 }
 
 output "sql_server_minimum_tls_version" {
-    value = azurerm_mssql_server.schroders.minimum_tls_version
+  value = azurerm_mssql_server.schroders.minimum_tls_version
 }
