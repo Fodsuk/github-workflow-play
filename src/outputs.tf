36c1cacf421c1e0927345a924a606ad78efcb693
fodsuk <fodsuk@hotmail.com>
2022-08-02 16:51:10 +0100

    build variables

--- src/outputs.tf
+++ src/outputs.tf
@@ -2,13 +2,13 @@ output "policies" {
   value = [for policy in azurerm_policy_definition.schroders : {
     name         = policy.name
     display_name = policy.display_name
+    mode         = policy.mode
   }]
 }
 
-output "policie_sets" {
+output "policy_sets" {
   value = [for policy in azurerm_policy_set_definition.schroders : {
     name         = policy.name
     display_name = policy.display_name
-    mode         = policy.mode
   }]
 }
---
+++ src/resource-policy_definition.tf
@@ -0,0 +1,21 @@
+resource "random_uuid" "policy_definition_name" {
+  for_each = var.policies
+}
+
+resource "azurerm_policy_definition" "schroders" {
+  for_each     = var.policies
+  name         = random_uuid.policy_definition_name[each.key].result
+  display_name = jsondecode(file("${path.module}/policies/${each.value.policy_file}"))["properties"]["displayName"]
+  description  = jsondecode(file("${path.module}/policies/${each.value.policy_file}"))["properties"]["description"]
+  policy_type  = jsondecode(file("${path.module}/policies/${each.value.policy_file}"))["properties"]["policyType"]
+  mode         = jsondecode(file("${path.module}/policies/${each.value.policy_file}"))["properties"]["mode"]
+
+  metadata    = jsonencode(jsondecode(file("${path.module}/policies/${each.value.policy_file}"))["properties"]["metadata"])
+  parameters  = jsonencode(jsondecode(file("${path.module}/policies/${each.value.policy_file}"))["properties"]["parameters"])
+  policy_rule = jsonencode(jsondecode(file("${path.module}/policies/${each.value.policy_file}"))["properties"]["policyRule"])
+
+  lifecycle {
+    //https://github.com/hashicorp/terraform-provider-azurerm/issues/15615
+    create_before_destroy = true
+  }
+}
