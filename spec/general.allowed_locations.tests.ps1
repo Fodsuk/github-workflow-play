8743aaf8e3f9804db464d32022a62369260e5b93
fodsuk <fodsuk@hotmail.com>
2022-08-18 14:15:35 +0100

    seperate vnet names

--- spec/general.allowed_locations.tests.ps1
+++ spec/general.allowed_locations.tests.ps1
@@ -13,16 +13,17 @@ Describe 'General Policies' -Tag "ci" {
         BeforeAll {
             $resource_group_id = $terraform_outputs.resource_group_policy_assignments_by_policy.value.allowed_locations[0].resource_group_id
             $resource_group_name = ($resource_group_id -replace '.*\/')
-            $vnet_name = -join ((97..122) | Get-Random -Count 18 | % { [char]$_ })
         }
 
         It 'Resources cant be created in disallowed locations' {
-           $outcome = ((az network vnet create --location EastUS --name $vnet_name --resource-group $resource_group_name --address-prefix 10.0.0.0/16 --subnet-name MySubnet --subnet-prefix 10.0.0.0/24 --output table)  2>&1 | Out-String)
+           $vnet_net = -join ((97..122) | Get-Random -Count 18 | % { [char]$_ })
+           $outcome = ((az network vnet create --location EastUS --name $vnet_net --resource-group $resource_group_name --address-prefix 10.0.0.0/16 --subnet-name MySubnet --subnet-prefix 10.0.0.0/24 --output table)  2>&1 | Out-String)
            $outcome | Should -Match "'You can only create resources in UKSouth or UKWest'"
         }
 
         It 'Resources can be created in allowed locations' {
-            $provisioning_state = az network vnet create --location UKSouth --name $vnet_name --resource-group $resource_group_name --address-prefix 10.0.0.0/16 --subnet-name MySubnet --subnet-prefix 10.0.0.0/24 --output tsv --query "newVNet.provisioningState"
+            $vnet_net = -join ((97..122) | Get-Random -Count 18 | % { [char]$_ })
+            $provisioning_state = az network vnet create --location UKSouth --name $vnet_net --resource-group $resource_group_name --address-prefix 10.0.0.0/16 --subnet-name MySubnet --subnet-prefix 10.0.0.0/24 --output tsv --query "newVNet.provisioningState"
             $provisioning_state | Should -Be Succeeded
          }
 
