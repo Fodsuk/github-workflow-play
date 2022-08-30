9bee2f791a28881262f902f8701629bd9e3ad9e7
Mike <fodsuk@hotmail.com>
2022-08-30 12:10:13 +0100

    remove tag (#19)
    * remove tag
    * policy scan on resource group

--- spec/general.allowed_locations.ci.tests.ps1
+++ spec/general.allowed_locations.ci.tests.ps1
@@ -14,10 +14,10 @@ Describe 'General Policies' -Tag "ci" {
             $resource_group_id = $terraform_outputs.resource_group_policy_assignments_by_policy.value.allowed_locations[0].resource_group_id
             $resource_group_name = ($resource_group_id -replace '.*\/')
 
-            Start-Sleep -Seconds 60 # race condition - policy assignments being created and checking they work.
+            az policy state trigger-scan -g $resource_group_name
         }
 
-        It 'Resources cant be created in disallowed locations' -Tag "mike" {
+        It 'Resources cant be created in disallowed locations' {
            $vnet_name = -join ((97..122) | Get-Random -Count 18 | % { [char]$_ })
            Write-Host "$resource_group_name / $vnet_name"
            $outcome = ((az network vnet create --location EastUS --name $vnet_name --resource-group $resource_group_name --address-prefix 10.0.0.0/16 --subnet-name MySubnet --subnet-prefix 10.0.0.0/24 2>&1 --verbose) | Out-String)
